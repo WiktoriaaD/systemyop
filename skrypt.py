8cd004f5ddfd4386ab44a2a5a7a24f5f5708cc11
Wikaa <dulskaw@gmail.com>
2025-10-22 21:02:05 +0200

    Update skrypt.py

--- skrypt.py
+++ skrypt.py
@@ -7,14 +7,13 @@ def list_processes():
     path_proc = "/proc"
     for item in os.listdir(path_proc):
         if item.isdigit():
-            pid = item
-            proc_path = os.path.join(path_proc, pid)
+            proc_path = os.path.join(path_proc, item)
             uid = os.stat(proc_path).st_uid
             user = pwd.getpwuid(uid).pw_name
 
             with open(os.path.join(proc_path, 'comm'), 'r') as f:
                     command = f.read().strip()
-            print(f"{user:<10} {pid:<10} {command}")
+            print(f"{user:<10} {item:<10} {command}")
 
 
 if __name__ == "__main__":
